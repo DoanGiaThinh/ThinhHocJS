71360edd9797c2658e233d5de5a4c90537914fbe
Thinh Hoc JS <bonemqst@gmail.com>
2026-03-28 20:59:30 +0700

    chapter07 bai 31

---
+++ src/chapter07/bai31/bai31.js
@@ -0,0 +1,29 @@
+console.log("Bài 31")
+
+// console.log("1")
+
+// //async
+// setTimeout(() => {
+//     console.log("2")
+// }, 5000)
+
+// console.log("3")
+
+const myPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("2 with promise")
+            //return
+            resolve("thinhdeptrai")
+        }, 2000)
+    })
+}
+
+const test = myPromise()
+console.log(test)
+console.log("====================================")
+console.log("1")
+myPromise().then(data => {
+    console.log("data:", data)
+    console.log("3")
+})
